a731ab3f5563383159503e79bf9ee14295403df7
Christian Daniel <cdc143@gmail.com>
2016-10-19 12:13:25 -0600

    Updating test information

--- test/test04.asm
+++ test/test04.asm
@@ -2,12 +2,12 @@
   org $1001               ; Unexpanded VIC
 
 ; BASIC stub (unexpanded vic)
-dc.w $100b              ; Pointer to next BASIC line
-dc.w 1981               ; BASIC Line#
-dc.b $9e                ; BASIC SYS token
-dc.b $34,$31,$30,$39    ; 4109 (ML start)
-dc.b 0                  ; End of BASIC line
-dc.w 0                  ; End of BASIC program
+  dc.w $100b              ; Pointer to next BASIC line
+  dc.w 1981               ; BASIC Line#
+  dc.b $9e                ; BASIC SYS token
+  dc.b $34,$31,$30,$39    ; 4109 (ML start)
+  dc.b 0                  ; End of BASIC line
+  dc.w 0                  ; End of BASIC program
 
 getRandom:
   lda #0 ;clear the accumulator
