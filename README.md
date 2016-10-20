72ceff554fdb6b00727cfef5440ce169ad5b9699
Christian Daniel <cdc143@gmail.com>
2016-10-19 22:34:57 -0600

    Clean up
    - Relocated contents of tests.txt to main realm
    -Added instructions on how to assemble and run programs
    -Added folder for prg files

--- README.md
+++ README.md
@@ -7,4 +7,19 @@ To assemble any of the test programs, cd to the test directory and then run:
 dasm [test_name].asm -o[test_name].prg where [test_name] is the name of the test you wish to assemble
 Then, run the test program by dragging the resulting prg file onto the xvic emulator window, double clicking the prg file, or running xvic [test_name].prg from terminal.
 
-Alternatively, run any of the pre-assembled prg files in the prg directory in the same fashion as above (without needing to assemble them) 
+Alternatively, run any of the pre-assembled prg files in the prg directory in the same fashion as above (without needing to assemble them)
+
+Test Programs:
+Test 00 - Simple return statement program
+Test 01 - Simple printing (prints VIC)
+Test 02 - Simple loop using printing algorithm in Test 01
+Test 03 - Simple program that takes W,A,S,D keyboard input and outputs a value depending on key pressed. Use Q to quit program.
+Test 04 - Simple random number generation using a hardcoded seed value and data from one of the VIC registers. Outputs a character (changes if you change the seed) then quits
+Test 05 - Program that changes text, background, and border colour of VIC 20 console
+Test 06 - Simple sound test with keyboard input.  Uses W,A,S,D to play sounds, Q to quit.
+Test 07 - Changing the character layout of the vic-20. We can use this for graphics or level design. By defining our own graphics characters. For example, try hitting m (shows up as a black square instead of an M). Use enter to quit
+Test 08 - Simple mirrored play field using characters
+Test 09 - Tests basic left/right character movement, based on code from tests 03 and 07.  Uses A and S as input to move left/right.
+Test 09-1 - Simple collision detection, extension of test 09.  Use W,A,S,D to move, Q to quit.
+Test 09-2 - Expanded test 09, included movement of up and down (W and S).  Only goes from 0-255, so not entire screen.  Use W,A,S,D to move, Q to quit.  Todo: expand to full screen.
+Test 10-1 - Simple collision animation based off Test 09-2.  Character 'B' changes colour and bounces back when it collides with 'C'. W,A,S,D to move, Q to quit.
--- test/test06.asm
+++ test/test06.asm
@@ -1,4 +1,4 @@
-  ;This test tests basic sound 
+;This test tests basic sound using W,A,S,D keys for user input to choose sound.
   ; vic pages used (page 98-102, 175)
   ;Oct 9, 2016
   processor 6502
@@ -16,11 +16,13 @@
  ldx #$01		 ; load 1 into x reg
  lda #$0f		 ; 15
  sta $900e		; set sound bits/turn on volume (see)
- lda #$00	
- sta $900b		; initialize speaker
+
+ ;note: $900a - low freq
+ ; $900b - med freq
+ ; $900c - high freq
  
 top:
-  ldy #$0f		 ; duration of sound (15 loops)
+  ;ldy #$0f		 ; duration of sound (15 loops)
   lda $00c5		 ; current key held down -> page 179 of vic20 manual
   cmp #17		 ;a pressed
   beq playleft		 
@@ -35,65 +37,78 @@ top:
 ;all these branches jump to their respective print-to-screen branch
 ;and then play to the branch "next"
 playleft:
-  jsr left
-  cpx #$00		; basically a branch always scenario because X is 1
-  bne next
+  jsr turnoff   ; turn off previous sound
+  jsr left      ; play sound
+  jmp next
   
 playright:
+  jsr turnoff
   jsr right
-  cpx #$00
-  bne next
+  jmp next
   
 playup:
+  jsr turnoff
   jsr up
-  cpx #$00
-  bne next
+  jmp next
   
 playdown:
+  jsr turnoff
   jsr down
   
 next:
   cmp #48		 ; check if Q is pressed -> quit
   bne top		 ;continue input
-  lda #$00		
-  sta $900b		 ; turn off sound
+  jsr turnoff ;turn off all sounds before exiting
   rts			 ; quit
   
 ;These subroutines print the next letter of W,A,S,D (X,B,T,E) to make sure
 ;that we aren't just seeing W,A,S,D being typed without the code 
 ;working.
 left:
-  lda #$87	 ; C  (135)
+  lda #$f0	 ; C  (240) high C
+  sta $900c
+  
+  lda #$87	 ; C  (135) low C
   ;jsr $ffd2	
-  sta $900b		 ; store sound
-  jsr timer		 ; jump to loop for duration of sound
+  sta $900a		 ; store sound
+  ;lda #$e1			; C 225
+  ;sta $900b
+  ;jsr timer		 ; jump to loop for duration of sound
   rts			 ; done
   
 right:
   lda #$9f		 ; E (159)
-  sta $900b			;store sound
-  jsr timer
+  sta $900c			;store sound
+  ;jsr timer
   ;jsr $ffd2
   rts			 ; done
   
 up:
   lda #$af		 ; G (175)
   ;jsr $ffd2
-  sta $900b			 ; store sound
-  jsr timer
+  sta $900a     ; store G in all registers, why not
+  sta $900b
+  sta $900c			 ; store sound
+  
+  ;jsr timer
   rts			 ; done
   
 down:
   lda #$b7		 ; A (183)
   ;jsr $ffd2
-  sta $900b 	 ;store sound
-  jsr timer
+  sta $900c 	 ;store sound
+  sta $900a		; have low and high sounds play
+ ; jsr timer
   rts			 ; done
   
-timer:			; plays sound for certain amount of time
-  dey
-  cpy #$00
-  bne timer
-  lda #$00
-  sta $900b 	; turn off sound
-  rts
+;timer:			; plays sound for certain amount of time
+;  dey
+;  cpy #$00
+;  bne timer
+;  rts
+turnoff:	; turn off sound for all registers
+ lda #$00
+ sta $900a
+ sta $900b
+ sta $900c
+ rts
