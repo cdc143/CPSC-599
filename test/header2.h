e621075c2c81e0e3e12b6ce0f41710ba1c8fd912
Christian Daniel <cdc143@gmail.com>
2016-12-07 14:18:58 -0700

    Score working again

--- test/header2.h
+++ test/header2.h
@@ -8,15 +8,15 @@ status_colour_top equ $9600
 status_colour_bot equ $97ce
 status_loc_top equ $1e00
 status_loc_bot equ $1fce
-space_sprite = #$04
-wall_sprite = #$05
-portal_sprite = #$07
-p1_sprite = #$01
-lives_sprite = #$09		;heart
-door_sprite = #$06
-enemy_sprite = #$02		;circle
+space_sprite = #$0e
+wall_sprite = #$0f
+portal_sprite = #$11
+p1_sprite = #$0b
+lives_sprite = #$13		;heart
+door_sprite = #$10
+enemy_sprite = #$0c		;circle
 
-sword_sprite = #$08
+sword_sprite = #$12
 w = #9
 a = #17
 s = #41
@@ -30,7 +30,7 @@ wall_colour = #$44
 life_colour = 2
 screen_colour = #$0f
 row_end = #$15
-;score_init = #$30
+;score_init = #$00
 init_lives = #$08
 row_begin = #$00
 row_newLevel_begin = #$01
